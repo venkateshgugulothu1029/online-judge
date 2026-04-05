8b6537b3b4236c882661949cda98d31a477a3a4d
venkateshgugulothu1029 <venkateshgugulothu1029@gmail.com>
2026-04-05 21:18:43 +0530

    Update executor.py

--- executor.py
+++ executor.py
@@ -2,19 +2,26 @@ import subprocess
 import tempfile
 
 def run_code(user_code, input_data):
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as temp:
-        temp.write(user_code.encode())
-        temp.flush()
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as temp:
+            temp.write(user_code.encode())
+            temp.flush()
 
-        result = subprocess.run(
-            ["python", temp.name],
-            input=input_data,
-            text=True,
-            capture_output=True,
-            timeout=5
-        )
+            result = subprocess.run(
+                ["python", temp.name],
+                input=input_data,
+                text=True,
+                capture_output=True,
+                timeout=3
+            )
 
-        return result.stdout.strip()
+            if result.stderr:
+                return result.stderr.strip()
+
+            return result.stdout.strip()
+
+    except subprocess.TimeoutExpired:
+        return "Execution Timed Out"
 
 def check_output(user_output, expected_output):
     return user_output.strip() == expected_output.strip()
